6e4485157ead35ff1b74f5e54492cb728d5ced9e
Tuang Keman <okkyituokky@gmail.com>
2026-05-13 11:45:20 +0700

    Update cobacoba.py

--- cobacoba.py
+++ cobacoba.py
@@ -16,3 +16,24 @@ hasil = (y1 - y2) // (x1 - x2)
 
 
 print(round(hasil))
+
+## network
+
+
+# network
+inputip = eval(input())
+
+inputsub = eval(input())
+
+pecah1,pecah2,pecah3,pecah4 = inputip
+
+acah1,acah2,acah3,acah4 = inputsub
+
+print(f"IP Address : ({pecah1}, {pecah2}, {pecah3}, {pecah4})")
+
+print(f"Subnet Mask : ({acah1}, {acah2}, {acah3}, {acah4})")
+
+print(f"Network Address : ({pecah1 & acah1}, {pecah2 & acah2}, {pecah3 & acah3}, {pecah4 & acah4})")
+
+print(f"Broadcast Address : ({(pecah1 & acah1) | (255 - acah1)}, {(pecah2 & acah2) | (255 - acah2)}, {(pecah3 & acah3) | (255 - acah3)}, {(pecah4 & acah4) | (255 - acah4)})")
+
