9b395d3da62065a9a127d942600d243a9f5978d9
Tuang Keman <okkyituokky@gmail.com>
2026-05-13 11:06:03 +0700

    Update cobacoba.py

--- cobacoba.py
+++ cobacoba.py
@@ -1,1 +1,18 @@
-###
+### slope
+
+input1 = input()
+input2 = input()
+
+input1 = input1.split()
+
+input2 = input2.split()
+
+y1, y2 = int(input2[1]), int(input1[1])
+
+x1, x2 = int(input2[0]), int(input1[0])
+
+
+hasil = (y1 - y2) // (x1 - x2)
+
+
+print(round(hasil))
