16a1278e79e2fa7b6964eeba858072989536f6ce
Tuang Keman <okkyituokky@gmail.com>
2026-05-26 17:43:44 +0700

    Update cobacoba.py

--- cobacoba.py
+++ cobacoba.py
@@ -1,1 +1,19 @@
-###
+from functools import cache
+@cache
+
+def f(n):
+    if n == 1:
+        return 1
+    else:
+        k = int(n/2)
+        o = n-k
+        return f(k) + f(o) + 1
+    
+
+n = int(input())
+isian = []
+
+for i in range(n):
+    isian.append(int(input()))
+for i in isian:
+    print(f(i))
